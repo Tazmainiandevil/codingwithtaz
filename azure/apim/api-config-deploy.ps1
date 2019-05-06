2611695f89adf6a58fcc87ebdc88eb426732fe97
Mark Johnson <mark@twistersdomain.co.uk>
2019-05-06 17:47:12 +0100

    Initial APIM templates

---
+++ azure/apim/api-config-deploy.ps1
@@ -0,0 +1,10 @@
+﻿$ResourceGroup = "<your apim resource group>"
+$DeploymentName = "apim-api-deploy"
+$Template = "api-config.json"
+$TemplateParameters = "api-config-parameters.json"
+$Subscription = "<your subscription>"
+
+Connect-AzureRmAccount
+Select-AzureRmSubscription -SubscriptionName $Subscription
+
+New-AzureRmResourceGroupDeployment -Name $DeploymentName -ResourceGroupName $ResourceGroup -TemplateFile $Template -TemplateParameterFile $TemplateParameters -DeploymentDebugLogLevel None
---
+++ azure/apim/product-config-deploy.ps1
@@ -0,0 +1,10 @@
+﻿$ResourceGroup = "<your apim resource group>"
+$DeploymentName = "apim-product-deploy"
+$Template = "product-config.json"
+$TemplateParameters = "product-config-parameters.json"
+$Subscription = "<your subscription>"
+
+Connect-AzureRmAccount
+Select-AzureRmSubscription -SubscriptionName $Subscription
+
+New-AzureRmResourceGroupDeployment -Name $DeploymentName -ResourceGroupName $ResourceGroup -TemplateFile $Template -TemplateParameterFile $TemplateParameters -DeploymentDebugLogLevel None
