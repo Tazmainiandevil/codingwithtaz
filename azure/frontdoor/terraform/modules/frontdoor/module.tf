13641cdbe86c3c6424eeb0e84910130f5154c173
Mark Johnson <mark@twistersdomain.co.uk>
2020-06-13 19:12:20 +0100

    Create module.tf

---
+++ azure/frontdoor/terraform/modules/frontdoor/module.tf
@@ -0,0 +1,103 @@
+# Create a resource group
+resource "azurerm_resource_group" "instance" {
+  name     = var.frontdoor_resource_group_name
+  location = var.frontdoor_resource_group_location
+}
+
+# Create front door
+resource "azurerm_frontdoor" "instance" {
+  name                                         = var.frontdoor_name
+  resource_group_name                          = azurerm_resource_group.instance.name
+  enforce_backend_pools_certificate_name_check = var.enforce_backend_pools_certificate_name_check
+  load_balancer_enabled                        = var.frontdoor_loadbalancer_enabled
+  backend_pools_send_receive_timeout_seconds   = var.backend_pools_send_receive_timeout_seconds
+  tags                                         = var.tags
+
+  dynamic "backend_pool_load_balancing" {
+    for_each = var.frontdoor_loadbalancer
+    content {
+      name                            = backend_pool_load_balancing.value.name
+      sample_size                     = backend_pool_load_balancing.value.sample_size
+      successful_samples_required     = backend_pool_load_balancing.value.successful_samples_required
+      additional_latency_milliseconds = backend_pool_load_balancing.value.successful_samples_required
+    }
+  }
+
+  dynamic "routing_rule" {
+    for_each = var.frontdoor_routing_rule
+    content {
+        name               = routing_rule.value.name
+        accepted_protocols = routing_rule.value.accepted_protocols
+        patterns_to_match  = routing_rule.value.patterns_to_match
+        frontend_endpoints = routing_rule.value.frontend_endpoints
+        dynamic "forwarding_configuration" {
+          for_each = routing_rule.value.configuration == "Forwarding" ? routing_rule.value.forwarding_configuration : []
+          content {
+            backend_pool_name                     = forwarding_configuration.value.backend_pool_name
+            cache_enabled                         = forwarding_configuration.value.cache_enabled                           
+            cache_use_dynamic_compression         = forwarding_configuration.value.cache_use_dynamic_compression #default: false
+            cache_query_parameter_strip_directive = forwarding_configuration.value.cache_query_parameter_strip_directive
+            custom_forwarding_path                = forwarding_configuration.value.custom_forwarding_path
+            forwarding_protocol                   = forwarding_configuration.value.forwarding_protocol
+          }
+        }
+        dynamic "redirect_configuration" {
+          for_each = routing_rule.value.configuration == "Redirecting" ? routing_rule.value.redirect_configuration : []
+          content {
+            custom_host         = redirect_configuration.value.custom_host
+            redirect_protocol   = redirect_configuration.value.redirect_protocol
+            redirect_type       = redirect_configuration.value.redirect_type
+            custom_fragment     = redirect_configuration.value.custom_fragment
+            custom_path         = redirect_configuration.value.custom_path
+            custom_query_string = redirect_configuration.value.custom_query_string
+          }
+        }
+    }
+  }
+
+ dynamic "backend_pool_health_probe" {
+    for_each = var.frontdoor_health_probe
+    content {
+      name                = backend_pool_health_probe.value.name
+      enabled             = backend_pool_health_probe.value.enabled
+      path                = backend_pool_health_probe.value.path
+      protocol            = backend_pool_health_probe.value.protocol
+      probe_method        = backend_pool_health_probe.value.probe_method
+      interval_in_seconds = backend_pool_health_probe.value.interval_in_seconds
+    }  
+  }
+
+  dynamic "backend_pool" {
+    for_each = var.frontdoor_backend
+    content {
+       name                = backend_pool.value.name      
+       load_balancing_name = backend_pool.value.loadbalancing_name
+       health_probe_name   = backend_pool.value.health_probe_name
+
+       dynamic "backend" {
+        for_each = backend_pool.value.backend
+        content {
+          enabled     = backend.value.enabled
+          address     = backend.value.address
+          host_header = backend.value.host_header
+          http_port   = backend.value.http_port
+          https_port  = backend.value.https_port
+          priority    = backend.value.priority
+          weight      = backend.value.weight
+        }
+      }
+    }
+  }
+
+  frontend_endpoint {
+    name                              = "${var.frontdoor_name}FrontendEndpoint"
+    host_name                         = "${var.frontdoor_name}.azurefd.net"
+    custom_https_provisioning_enabled = var.custom_https_provisioning_enabled    
+    dynamic custom_https_configuration {
+      for_each = var.custom_https_provisioning_enabled == false ? [] : list(var.custom_https_configuration.certificate_source)
+      content {
+        certificate_source = custom_https_configuration.value.certificate_source
+      }
+    }
+  }
+}
